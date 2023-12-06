41ffc711ed4dd2c2171652fad1504f5ce410c8b6
Sarvesh Yadav <sarunits7@gmail.com>
2023-12-07 01:21:10 +0530

    defined routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('login', function(){
+    return view('login');
+});
+Route::get('signup', function(){
+    return view('signup');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/login',[AuthController::class,'login'])->name('login');
+Route::post('/signup',[CustomerController::class,'signup'])->name('signup');

e1a89ea5dff3e85ebf20f135ec9d8503b3ef8e63
Sarvesh Yadav <sarunits7@gmail.com>
2023-12-07 01:19:25 +0530

    create customer table using migration

---
+++ database/migrations/2023_12_06_152551_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('email',255);
+            $table->string('password');
+            $table->string('mobile',15);
+            $table->enum('gender',['male','female','other']);
+            $table->string('state',255);
+            $table->string('district',255);
+            $table->enum('status',['0','1']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};

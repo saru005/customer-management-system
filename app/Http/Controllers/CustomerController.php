594973be3dee31abe0d6eadd37aa591d00b4d09c
Sarvesh Yadav <sarunits7@gmail.com>
2023-12-07 18:00:20 +0530

    all customer crud operation written in CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Repositories\CustomerRepositoryInterface;
@@ -15,6 +16,50 @@ class CustomerController extends Controller
     }
     public function signup(Request $request)
     {
+        
+        $validator = Validator::make($request->all(), [
+            'cust_name' => 'required',
+            'email' => 'required|email',
+            'mobile' => 'required|max:15',
+            'state' => 'required',
+            'district' => 'required',
+            'gender' => 'required',
+            'password' => 'required|string|min:8',
+            'confirm_password' => 'required|string|same:password',
+        ]);
+        
+        if ($validator->fails()) 
+        {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        $customer = $this->CustomerRepository->createCustomer($request->all());
+        if($customer){
+            return response()->json(['success' => true,'message' => 'sign up successfully'], 200);
+        }
+        
+        return response()->json(['success' => false,'message' => 'Something went wrong'], 500);
+    }
+
+    public function approveCustomer($id)
+    {
+        $customer = $this->CustomerRepository->updateCustomerStatus($id,'1');
+        return back()->with(['status'=>true,'message' => 'Customer Approved'], 200);
+        
+    }
+
+    public function rejectCustomer($id)
+    {
+        $customer = $this->CustomerRepository->updateCustomerStatus($id,'0');
+        return back()->with(['status'=>true,'message' => 'Customer Approved'], 200);  
+    }
+
+    public function editCustomerPage($id)
+    {
+        $customer = $this->CustomerRepository->getCustomerById($id);
+        return view('admin.Customer_edit',compact('customer'));
+    }
+    
+    public function edit(Request $request,$id){
         $validator = Validator::make($request->all(),[
             'cust_name' => 'required',
             'email' => 'required|email',
@@ -22,12 +67,14 @@ class CustomerController extends Controller
             'state' => 'required',
             'district' => 'required',
             'gender' => 'required',
-            'password' => 'required|string|confirmed',
         ]);
         if ($validator->fails()) 
         {
             return response()->json(['errors' => $validator->errors()], 422);
         }
-        $this->CustomerRepository->createCustomer($request->all());
+        $customer = $this->CustomerRepository->updateCustomer($request->all(),$id);
+        if($customer){
+            return response()->json(['success' => true, 'redirect' => route('dashboard'),'message' => 'Customer Updated Successfully']);
+        }
     }
 }

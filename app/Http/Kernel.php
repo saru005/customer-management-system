b2c8be88e82664b95f75f09b4fb3d87ca8d7dbd4
Sarvesh Yadav <sarunits7@gmail.com>
2023-12-07 17:57:09 +0530

    Register admin auth middleware in kernal file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,4 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+    protected $routeMiddleware = [
+        'admin.auth' => \App\Http\Middleware\AdminAuth::class,
+    ]; 
 }

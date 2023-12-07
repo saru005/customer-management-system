6fdfbad107baf062dbb20991f3eeeee61979a56f
Sarvesh Yadav <sarunits7@gmail.com>
2023-12-07 17:58:32 +0530

    return admin panel dashboard from adminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\CustomerRepositoryInterface;
 
 class AdminController extends Controller
 {
-    //
+    protected $CustomerRepository;
+    public function __construct(CustomerRepositoryInterface $CustomerRepository)
+    {
+        $this->CustomerRepository = $CustomerRepository;
+    }
+    public function index()
+    {
+        $customers = $this->CustomerRepository->getAllCustomer();
+        return view('admin.index', compact('customers'));
+    }
 }

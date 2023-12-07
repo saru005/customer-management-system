11f4f600061e98aa46510a1ba5a9126cedf89376
Sarvesh Yadav <sarunits7@gmail.com>
2023-12-07 18:02:43 +0530

    all repository function declared in this file

--- app/Repositories/CustomerRepositoryInterface.php
+++ app/Repositories/CustomerRepositoryInterface.php
@@ -4,4 +4,8 @@ namespace app\Repositories;
 interface CustomerRepositoryInterface
 {
     public function createCustomer($data);
+    public function getAllCustomer();
+    public function getCustomerById($id);
+    public function updateCustomerStatus($id,$status);
+    public function updateCustomer($data,$id);
 }
